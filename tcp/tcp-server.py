c694c8d376395ce7fd66ac099567d3fc093b08ef
Hasan <hasan.bilgin@ozu.edu.tr>
2020-12-22 00:17:56 +0300

    added the first version of server-side tcp socket programming file

--- tcp/tcp-server.py
+++ tcp/tcp-server.py
@@ -1,1 +1,24 @@
+import socket
 
+#ip address and port number for the connection
+serverip = None
+serverport = 12000
+
+#creating a socket and listening for connections
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind((serverIP, serverPort))
+serversocket.listen(1)
+
+	
+while True:
+    #accepting incoming connection
+	clientsocket, address = serversocket.accept()
+	
+    clientsocket.send(bytes("Sending file...", "utf-8"))
+
+    #will be filled later
+
+    clientsocket.send(bytes('Transfer complete', 'utf-8'))
+
+    #closing the connection
+    clientsocket.close()
