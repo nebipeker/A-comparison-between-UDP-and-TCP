f042af60a24f83c0423f787b03869b8624b5c6ad
Hasan <hasan.bilgin@ozu.edu.tr>
2020-12-24 00:21:33 +0300

    wrote the file transfer part

--- tcp/tcp-server.py
+++ tcp/tcp-server.py
@@ -13,12 +13,23 @@ serversocket.listen(1)
 while True:
     #accepting incoming connection
 	clientsocket, address = serversocket.accept()
-	
-    clientsocket.send(bytes("Sending file...", "utf-8"))
 
-    #will be filled later
+    print('Connected to %s' % address)
+	
+    clientsocket.send(bytes('Sending file...', 'utf-8'))
+
+    filename = 'data.txt' #must be in the same folder with tcp-server.py
+    testfile = open(filename, 'rb') #r is for reading and b is for binary, 'reading binary file'
+    #read the next 1024 bytes from the file
+    data = testfile.read(1024)
+    #when there is data left send it
+    while (data):
+       clientsocket.send(data)
+       data = testfile.read(1024)
+    testfile.close()
+    print('File is sent')
 
     clientsocket.send(bytes('Transfer complete', 'utf-8'))
 
-    #closing the connection
+    #closing the connection with client
     clientsocket.close()
