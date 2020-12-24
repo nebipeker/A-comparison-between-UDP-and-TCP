4906c08e4a2990234f5833c68ee1c022de021efa
Peker Çelik <44347418+nebipeker@users.noreply.github.com>
2020-12-24 23:38:28 +0300

    Main added

---
+++ tcp/main.py
@@ -0,0 +1,6 @@
+import tcp.tcp_client
+import tcp.tcp_server
+
+
+tcp.tcp_server.run()
+tcp.tcp_client.run()
--- tcp/tcp_client.py
+++ tcp/tcp_client.py
@@ -1,23 +1,23 @@
 import socket
 
+def run():
+    # Host ip adress and port
+    port = 12000
+    host = "192.168.56.1"
 
-# Host ip adress and port
-port = 12000
-host = "192.168.56.1"
+    #Connection and saving location
+    socket_ = socket.socket()
+    socket_.connect((host, port))
+    f = open("/Users/alpha/Documents/GitHub/CS447/test.txt",'wb')
 
-#Connection and saving location
-socket_ = socket.socket()
-socket_.connect((host, port))
-f = open("/Users/alpha/Desktop/CS447/test.txt",'wb')
-
-#Getting data
-while True:
-    try:
-        print("Receiving data...")
-        data = socket_.recv(1024)
-    except:
-        print("Error at recieving!!")
-    if not data:
-        break
-    print("Data=",data)
-    f.write(data)
+    #Getting data
+    while True:
+        try:
+            print("Receiving data...")
+            data = socket_.recv(1024)
+        except:
+            print("Error at recieving!!")
+        if not data:
+            break
+        print("Data=",data)
+        f.write(data)
