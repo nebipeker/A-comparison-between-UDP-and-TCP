b1e5855d2309a799ecd5c47424cac482f8f91db1
Hasan <hasan.bilgin@ozu.edu.tr>
2020-12-24 23:10:11 +0300

    fixed an error

--- tcp/tcp-server.py
+++ tcp/tcp-server.py
@@ -1,11 +1,7 @@
 import socket
 
 #ip address and port number for the connection
-<<<<<<< HEAD
 serverip = ''
-=======
-serverip = '85.103.110.149'
->>>>>>> 5f1585a361933711cdbd14b8f707f048a9c0af89
 serverport = 12000
 
 #creating a socket and listening for connections
@@ -17,11 +13,6 @@ serversocket.listen(1)
 while True:
     #accepting incoming connection
 	clientsocket, address = serversocket.accept()
-<<<<<<< HEAD
-=======
-
-    #print('Connected to {adress}'.format(adress))
->>>>>>> 5f1585a361933711cdbd14b8f707f048a9c0af89
 	
     clientsocket.send(bytes('Sending file...', 'utf-8'))
 
