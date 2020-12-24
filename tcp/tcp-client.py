cd68098877da72d6d65861d56a00e4d0667f1f95
Peker Çelik <44347418+nebipeker@users.noreply.github.com>
2020-12-24 14:48:15 +0300

    tcp-client.py added

---
+++ tcp/tcp-client.py
@@ -0,0 +1,21 @@
+import socket
+
+# Host ip adress and port
+port = 8081
+host = None
+
+#Connection and saving location
+s = socket.socket()
+s.connect((host, port))
+f = open("/Users/alpha/Desktop/CS447/test.txt",'wb')
+
+#Getting data
+while True:
+    try:
+        print("Receiving data...")
+        data = s.recv(1024)
+
+    if not data:
+        break
+    print("Data=%s",  (data))
+    f.write(data)
