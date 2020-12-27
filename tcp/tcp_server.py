95726efcef2ddd1de511037d6d49689ab3805814
Hasan <hasan.bilgin@ozu.edu.tr>
2020-12-27 21:07:42 +0300

    adjusted a few lines

--- tcp/tcp_server.py
+++ tcp/tcp_server.py
@@ -1,33 +1,31 @@
 import socket
-def run():
-    #ip address and port number for the connection
-    serverip = ''
-    serverport = 12000
+#ip address and port number for the connection
+serverip = ''
+serverport = 12000
 
     #creating a socket and listening for connections
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind((serverip, serverport))
-    serversocket.listen(1)
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+serversocket.bind((serverip, serverport))
+serversocket.listen(1)
 
 
-    while True:
-        #accepting incoming connection
-        clientsocket, address = serversocket.accept()
+while True:
+    #accepting incoming connection
+    clientsocket, address = serversocket.accept()
 
-        clientsocket.send(bytes('Sending file...', 'utf-8'))
+    print('Sending file...')
 
-        filename = 'data.txt' #must be in the same folder with tcp-server.py
-        testfile = open(filename, 'rb') #r is for reading and b is for binary, 'reading binary file'
-        #read the next 1024 bytes from the file
-        data = testfile.read(1024)
-        #when there is data left send it
-        while (data):
-           clientsocket.send(data)
-           data = testfile.read(1024)
-        testfile.close()
-        print('File is sent')
-
-        clientsocket.send(bytes('Transfer complete', 'utf-8'))
-
-        #closing the connection with client
-        clientsocket.close()
+    filename = 'data.txt' #must be in the same folder with tcp-server.py
+    testfile = open(filename, 'rb') #r is for reading and b is for binary, 'reading binary file'
+    #read the next 1024 bytes from the file
+    data = testfile.read(1024)
+    #when there is data left send it
+    while (data):
+       clientsocket.send(data)
+       data = testfile.read(1024)
+    testfile.close()
+    print('File is sent')
+        
+    #closing the connection with client
+    clientsocket.close()
