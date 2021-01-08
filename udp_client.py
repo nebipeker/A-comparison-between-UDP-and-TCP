956c6a712ef2a95d1b67532f68dd445656ac549d
Yamaç Demirkan Yılmaz <yamac.demirkan@protonmail.ch>
2021-01-08 22:32:27 +0300

    Updated UDPClient

--- udp_client.py
+++ udp_client.py
@@ -3,6 +3,6 @@ import base64
 
 udpclient = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 udpclient.sendto(str.encode("UDP MESSAGE"), ("127.0.0.1", 20001))
-message = udpclient.recvfrom(1024)
+message = udpclient.recvfrom(200000)
 message_bytes = base64.b64decode(message[0])
 print((message_bytes.decode('ascii')))
