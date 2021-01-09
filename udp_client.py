cf5dcef276b833486c04676c06ef7edcfae9d149
Yamaç Demirkan Yılmaz <yamac.demirkan@protonmail.ch>
2021-01-09 17:05:02 +0300

    updated the udpclient and udpserver

--- udp_client.py
+++ udp_client.py
@@ -3,14 +3,16 @@ import base64
 
 from sys import getsizeof
 
-def chunks(maxBufferSize, listOfLines):
+def chunks(maxBufferSize, inputlines):
     toSend = []
+    listOfLines = inputlines
     while listOfLines:
         if(toSend):
             if((getsizeof(listOfLines[0])+getsizeof(toSend[-1])) <= maxBufferSize):
                 toSend[-1] = toSend[-1] + listOfLines.pop(0)
             else:
-                toSend.append(listOfLines.pop(0))
+                #toSend.append(listOfLines.pop(0))
+                listOfLines.pop(0)
         else:
             toSend.append(listOfLines.pop(0))
     return toSend
@@ -22,10 +24,12 @@ with open(FILENAME) as f:
     lines = f.readlines()
 
 groupLines = chunks(BUFFER_SIZE, lines)
+print(list(map(getsizeof,groupLines)))
+print(groupLines)
 
 for line in groupLines:
     udpclient = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    udpclient.sendto(str.encode(line), ("127.0.0.1", 20001))
+    udpclient.sendto(str.encode(line), ("127.0.0.1", 20005))
     message = udpclient.recvfrom(BUFFER_SIZE)
     message_bytes = base64.b64decode(message[0])
     print((message_bytes.decode('ascii')))
--- udp_server.py
+++ udp_server.py
@@ -3,10 +3,11 @@ import base64
 
 BUFFER_SIZE = 512
 
-content_bytes = base64.encodebytes("UDP SERVER LISTENING")
+content_bytes = base64.encodebytes(str.encode("UDP SERVER LISTENING"))
 udpserver = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-udpserver.bind(("127.0.0.1", 20001))
+udpserver.bind(("127.0.0.1", 20005))
 
+print("sadassfasfsfda")
 while(True):
     msg,ip = udpserver.recvfrom(BUFFER_SIZE)
     udpserver.sendto(content_bytes, ip)
