f27bccb31aafdf21141218217a55b11710ce8cb3
iscanegemen <egemen.iscan@ozu.edu.tr>
2021-01-12 20:48:41 +0300

    Major modifications for UDP server and client

--- test B - medium file/udp/udp_server.py
+++ test B - medium file/udp/udp_server.py
@@ -1,16 +1,29 @@
 import socket
 import base64
+from sys import getsizeof
 
 BUFFER_SIZE = 512
+FILENAME = 'data.txt'
 
-content_bytes = base64.encodebytes(str.encode("UDP SERVER"))
+def chunks(text, maxBufferSize):
+    i = 0
+    string = ''
+    while i < maxBufferSize:
+        string = string + '.'
+        i = getsizeof(string)
+    splitted = [text[0+m:len(string)+m] for m in range(0, len(text),len(string))]
+    return splitted
+
+toSend = open(FILENAME, 'rb').read()
+lines = chunks(toSend,BUFFER_SIZE)
 udpserver = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 udpserver.bind(("127.0.0.1", 20001))
 
 while(True):
-    msg,ip = udpserver.recvfrom(BUFFER_SIZE)
-    udpserver.sendto(content_bytes, ip)
-    print("UDP CLIENT MESSAGE:")
-    ##print(msg.decode('utf-8'))
-    print("CLIENT IP:",ip)
-
+    for num,line in enumerate(lines): 
+        msg,ip = udpserver.recvfrom(BUFFER_SIZE)
+        content_bytes = base64.encodebytes(line)
+        if(num == len(lines)-1):
+            content_bytes = base64.encodebytes(str.encode('UDP SERVER'))
+        udpserver.sendto(content_bytes, ip)
+        print("CLIENT IP:",ip)
--- test C - large file/udp/udp_client.py
+++ test C - large file/udp/udp_client.py
@@ -1,27 +1,15 @@
 import socket
 import base64
-from sys import getsizeof
 
-FILENAME = 'data.txt'
 BUFFER_SIZE = 512
+f = open("output.txt", 'w+')
+message_bytes = bytes()
 
-def chunks(text, maxBufferSize):
-    i = 0
-    string = ''
-    while i < maxBufferSize:
-        string = string + '.'
-        i = getsizeof(string)
-    print(text)
-    splitted = [text[0+m:len(string)+m] for m in range(0, len(text),len(string))]
-    return splitted
-
-toSend = open(FILENAME, 'rb').read()
-
-lines = chunks(toSend,BUFFER_SIZE)
-
-for line in lines:
+while(message_bytes.decode('ascii')!="UDP SERVER"):
     udpclient = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    udpclient.sendto(line, ("127.0.0.1", 20001))
+    udpclient.sendto(str.encode("UDP CLIENT MESSAGE"),("127.0.0.1", 20001))
     message = udpclient.recvfrom(BUFFER_SIZE)
     message_bytes = base64.b64decode(message[0])
-    print((message_bytes.decode('ascii')))
+    f.write(message_bytes.decode('ascii'))
+
+f.close()
